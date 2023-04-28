539d09ece97e1a0c6ed6f96723433fb85d619ba5
Jon Andrew <jonfandrew@fastmail.com>
2023-04-28 14:35:25 -0600

    MAAS init works now

--- zarf-package-maas/region-rack-image/addmaasuser.sh
+++ zarf-package-maas/region-rack-image/addmaasuser.sh
@@ -3,6 +3,5 @@
 # This script is used to add a user to the MAAS server
 # and initialize MAAS for use.
 
-maas init --admin-username admin --admin-password admin --admin-email 'x@x.com' --rbac_url="" --candid-agent-file=""
-maas createadmin --username admin --password admin --email "x@x.com"
+maas init --admin-username admin --admin-password admin --admin-email 'x@x.com' --rbac-url="" --candid-agent-file=""
 systemctl disable runonce.service
