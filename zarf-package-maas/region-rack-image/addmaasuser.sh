727464bf9b8b36512b4c286261d6660979182a71
Wayne Starr <me@racer159.com>
2023-06-08 16:44:05 -0500

    Switch to a snap

--- zarf-package-maas/region-rack-image/addmaasuser.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# This script is used to add a user to the MAAS server
-# and initialize MAAS for use.
-
-maas init --admin-username admin --admin-password admin --admin-email 'x@x.com' --rbac-url="" --candid-agent-file=""
-systemctl disable runonce.service
