c569981e28ddbc3a127c2843d9459cf9aaad4ec3
Lauren Mengert <lauren@defenseunicorns.com>
2023-06-08 16:46:22 -0600

    Finish Zarf dep download script

--- zarf-bootstrap-usb/maker/scripts/deps/zarf.sh
+++ zarf-bootstrap-usb/maker/scripts/deps/zarf.sh
@@ -5,40 +5,43 @@ OUT="$1"
 RELEASES="https://github.com/defenseunicorns/zarf/releases/download"
 VERSION="v0.27.1"
 REMOTE_INIT="${RELEASES}/${VERSION}/zarf-init-amd64-${VERSION}.tar.zst"
-REMOTE_BIN="${RELEASES}/${VERSION}/zarf_${VERSION}_linux_amd64"
+REMOTE_BIN="${RELEASES}/${VERSION}/zarf_${VERSION}_Linux_amd64"
 REMOTE_SUMS="${RELEASES}/${VERSION}/checksums.txt"
 
-LOCAL_SUMS="$OUT/$( basename "$REMOTE_SUMS" )"
+SUMS=$( basename "$REMOTE_SUMS" )
+LOCAL_SUMS="$OUT/$SUMS"
 if [ ! -f "$LOCAL_SUMS" ] ; then
   curl --location --output "$LOCAL_SUMS" "$REMOTE_SUMS"
 fi
 
-# ZARF_BIN=$( basename "$REMOTE_ZARF_BIN" )
-# if [ ! -f "$DL/$ZARF_BIN" ] ; then
-#   curl --location --output "$DL/$ZARF_BIN" "$REMOTE_ZARF_BIN"
-# fi
+BIN=$( basename "$REMOTE_BIN" )
+LOCAL_BIN="$OUT/$BIN"
+if [ ! -f "$LOCAL_BIN" ] ; then
+  curl --location --output "$LOCAL_BIN" "$REMOTE_BIN"
+fi
 
-# sumcheck=$( cd "$DL" ; cat "$ZARF_SUMS" | grep "$ZARF_BIN" | sha256sum --check )
-# if [ $? -ne 0 ] ; then
-#   echo ""
-#   echo "$ZARF_BIN checksum did not match!  Delete & retry!"
-#   echo ""
-#   exit 1
-# fi
-# echo "sha256sum: $sumcheck"
+sumcheck=$( cd "$OUT" ; cat "$SUMS" | grep "$BIN" | sha256sum --check )
+if [ $? -ne 0 ] ; then
+  echo ""
+  echo "$LOCAL_BIN checksum did not match!  Delete & retry!"
+  echo ""
+  exit 1
+fi
+echo "sha256sum: $sumcheck"
 
-# ZARF_INIT=$( basename "$REMOTE_ZARF_INIT" )
-# if [ ! -f "$DL/$ZARF_INIT" ] ; then
-#   curl --location --output "$DL/$ZARF_INIT" "$REMOTE_ZARF_INIT"
-# fi
+INIT=$( basename "$REMOTE_INIT" )
+LOCAL_INIT="$OUT/$INIT"
+if [ ! -f "$LOCAL_INIT" ] ; then
+  curl --location --output "$LOCAL_INIT" "$REMOTE_INIT"
+fi
 
-# sumcheck=$( cd "$DL" ; cat "$ZARF_SUMS" | grep "$ZARF_INIT" | sha256sum --check )
-# if [ $? -ne 0 ] ; then
-#   echo ""
-#   echo "$ZARF_INIT checksum did not match!  Delete & retry!"
-#   echo ""
-#   exit 1
-# fi
-# echo "sha256sum: $sumcheck"
+sumcheck=$( cd "$OUT" ; cat "$SUMS" | grep "$INIT" | sha256sum --check )
+if [ $? -ne 0 ] ; then
+  echo ""
+  echo "$LOCAL_INIT checksum did not match!  Delete & retry!"
+  echo ""
+  exit 1
+fi
+echo "sha256sum: $sumcheck"
 
-# chmod +x "$DL/$ZARF_BIN"
+chmod +x "$LOCAL_BIN"
