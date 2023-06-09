4ec70eb62885e2cc9ee03b21d0644509c36980d4
Barrett LaFrance <barrett@defenseunicorns.com>
2023-06-09 13:28:50 -0500

    wip: migrate zarf boots iso

--- zarf-bootstrap-usb/maker/scripts/build/zarf-boots-iso.sh
+++ zarf-bootstrap-usb/maker/scripts/build/zarf-boots-iso.sh
@@ -7,70 +7,66 @@ iso_original="$( compgen -G "$DEPS/ubuntu/ubuntu-*-amd64.iso" )"
 iso_unpacked="$OUT/zarf-boots.fs"
 iso_zarfboot="$OUT/zarf-boots.iso"
 
-# TODO: delete!
-echo "$iso_original"
-echo "$iso_unpacked"
-echo "$iso_zarfboot"
 
 # unpack original iso
-7z -y x -o"$iso_unpacked" "$iso_original"
+boot="[BOOT]"
+boot_imgs="$OUT/$boot"
+if [ ! -d "$iso_unpacked" ] ; then
+  7z -y x -o"$iso_unpacked" "$iso_original"
+  mv "$iso_unpacked/$boot" "$OUT"
+fi
 
 
-# # mv boot partition images up to working directory
-# mv "$unpack/[BOOT]" "$WD_ISO/BOOT"
+# inject autoinstall boot option
+grubcfg="$iso_unpacked/boot/grub/grub.cfg"
+
+read -r -d '' boot_option <<'EOF'
+menuentry "Install Zarf Boots!" {
+  set gfxpayload=keep
+  linux   /casper/vmlinuz quiet autoinstall ds=nocloud\;s=/cdrom/zarf/  ---
+  initrd  /casper/initrd
+}
+EOF
+needle=$( cat "$grubcfg" | awk '/Try or Install/{ print NR; exit }' )
+count=$( wc --lines "$grubcfg" | awk '{print $1}' )
+
+head=$( cat "$grubcfg" | head -n $(($needle - 1)) )
+tail=$( cat "$grubcfg" | tail -n $(($count - $needle + 1 )) )
+
+echo "$head" > "$grubcfg"
+echo "" >> "$grubcfg"
+echo "$boot_option" >> "$grubcfg"
+echo "" >> "$grubcfg"
+echo "$tail" >> "$grubcfg"
 
 
-# # inject autoinstall boot option
-# grubcfg="$unpack/boot/grub/grub.cfg"
-
-# read -r -d '' boot_option <<'EOF'
-# menuentry "Autoinstall Zarf Bootstrapper" {
-#   set gfxpayload=keep
-#   linux   /casper/vmlinuz quiet autoinstall ds=nocloud\;s=/cdrom/zarf/  ---
-#   initrd  /casper/initrd
-# }
-# EOF
-# needle=$( cat "$grubcfg" | awk '/Try or Install/{ print NR; exit }' )
-# count=$( wc --lines "$grubcfg" | awk '{print $1}' )
-
-# head=$( cat "$grubcfg" | head -n $(($needle - 1)) )
-# tail=$( cat "$grubcfg" | tail -n $(($count - $needle + 1 )) )
-
-# echo "$head" > "$grubcfg"
-# echo "" >> "$grubcfg"
-# echo "$boot_option" >> "$grubcfg"
-# echo "" >> "$grubcfg"
-# echo "$tail" >> "$grubcfg"
+# disable autoselection of destructive Zarf-paving menuentry
+sed -i 's/set timeout=30/set timeout=-1/' "$grubcfg"
 
 
-# # disable autoselection of destructive Zarf-paving menuentry
-# sed -i 's/set timeout=30/set timeout=-1/' "$grubcfg"
+# add autoinstall config directory
+AI="$iso_unpacked/zarf" ; mkdir -p "$AI"
+touch "$AI/meta-data"
+cp "./autoinstall.yaml" "$AI/user-data"
 
 
-# # add autoinstall config directory
-# AI="$unpack/zarf" ; mkdir -p "$AI"
-# touch "$AI/meta-data"
-# cp "./autoinstall.yaml" "$AI/user-data"
-
-
-# # pack modified iso
-# ZARF_ISO="$WD/zarf-boots.iso"
-# xorriso -as mkisofs -r \
-#   -V 'Zarf Boots' \
-#   -o "$ZARF_ISO" \
-#   --grub2-mbr "$WD_ISO/BOOT/1-Boot-NoEmul.img" \
-#   -partition_offset 16 \
-#   --mbr-force-bootable \
-#   -append_partition 2 28732ac11ff8d211ba4b00a0c93ec93b "$WD_ISO/BOOT/2-Boot-NoEmul.img" \
-#   -appended_part_as_gpt \
-#   -iso_mbr_part_type a2a0d0ebe5b9334487c068b6b72699c7 \
-#   -c '/boot.catalog' \
-#   -b '/boot/grub/i386-pc/eltorito.img' \
-#     -no-emul-boot -boot-load-size 4 -boot-info-table --grub2-boot-info \
-#   -eltorito-alt-boot \
-#   -e '--interval:appended_partition_2:::' \
-#   -no-emul-boot \
-#   "$unpack"
+# pack modified iso
+xorriso -as mkisofs -r \
+  -V 'Zarf Boots' \
+  -o "$iso_zarfboot" \
+  --grub2-mbr "$boot_imgs/1-Boot-NoEmul.img" \
+  -partition_offset 16 \
+  --mbr-force-bootable \
+  -append_partition 2 28732ac11ff8d211ba4b00a0c93ec93b "$boot_imgs/2-Boot-NoEmul.img" \
+  -appended_part_as_gpt \
+  -iso_mbr_part_type a2a0d0ebe5b9334487c068b6b72699c7 \
+  -c '/boot.catalog' \
+  -b '/boot/grub/i386-pc/eltorito.img' \
+    -no-emul-boot -boot-load-size 4 -boot-info-table --grub2-boot-info \
+  -eltorito-alt-boot \
+  -e '--interval:appended_partition_2:::' \
+  -no-emul-boot \
+  "$iso_unpacked"
 
 
 # # set img / & write block size (in bytes)
